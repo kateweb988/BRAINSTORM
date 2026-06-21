9764859764f4bb857a79eeebb8b48c0d0be6f70e
Vovas <koweb93@gmail.com>
2026-06-21 23:33:34 +0300

    ff

---
+++ dev/js/all.js
@@ -0,0 +1,273 @@
+
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        function (a) {
+          return "\\d{1," + a.length + "}"
+        }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+document.addEventListener('DOMContentLoaded', function () {
+  $('.articmodal-close').click(function (e) {
+    $.arcticmodal('close');
+
+  });
+  $('.a1').click(function (e) {
+    e.preventDefault();
+    $('#popup-call').arcticmodal({
+    });
+  });
+  $('.a2, .link').click(function (e) {
+    e.preventDefault();
+    $('#popup-call2').arcticmodal({
+    });
+  });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+  $(document).ready(function () {
+    $('[data-submit]').on('click', function (e) {
+      e.preventDefault();
+      $(this).parents('form').submit();
+    })
+    $.validator.addMethod(
+      "regex",
+      function (value, element, regexp) {
+        var re = new RegExp(regexp);
+        return this.optional(element) || re.test(value);
+      },
+      "Please check your input."
+    );
+    function valEl(el) {
+
+      el.validate({
+        rules: {
+          tel: {
+            required: true,
+            regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+          },
+          name: {
+            required: true
+          },
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          tel: {
+            required: 'Заполните поле',
+            regex: 'Телефон может содержать символы + - ()'
+          },
+          name: {
+            required: 'Заполните поле',
+          },
+          text: {
+            required: 'Заполните поле',
+          },
+          email: {
+            required: 'Заполните поле',
+            email: 'Неверный формат E-mail'
+          }
+        },
+        submitHandler: function (form) {
+          $('#loader').fadeIn();
+          var $form = $(form);
+          var $formId = $(form).attr('id');
+          switch ($formId) {
+            case 'popupResult':
+              $.ajax({
+                type: 'POST',
+                url: $form.attr('action'),
+                data: $form.serialize(),
+              })
+                .always(function (response) {
+                  setTimeout(function () {
+                    $('#loader').fadeOut();
+                  }, 800);
+                  setTimeout(function () {
+                    $.arcticmodal('close');
+                    $('#popup-thank').arcticmodal({});
+                    $form.trigger('reset');
+                    //строки для остлеживания целей в Я.Метрике и Google Analytics
+                  }, 1100);
+
+                });
+              break;
+          }
+          return false;
+        }
+      })
+    }
+
+    $('.js-form').each(function () {
+      valEl($(this));
+    });
+    $('[data-scroll]').on('click', function () {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'data-scroll')).offset().top
+      }, 2000);
+      event.preventDefault();
+    })
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper1 = new Swiper('.swiper1', {
+    slidesPerView: 5,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 16,
+        loop: true,
+        slidesPerView: 3
+      },
+      767: {
+        spaceBetween: 16,
+        slidesPerView: 3
+      },
+      992: {
+        spaceBetween: 16,
+        slidesPerView: 3
+      },
+      1200: {
+        spaceBetween: 16,
+        slidesPerView: 5
+      }
+    }
+  });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".swiper1 .swiper-slide");
+    const catalogs = document.querySelectorAll(".catalog__open");
+
+    // Производители
+    catalogs.forEach((item, index) => {
+        item.style.display = index === 0 ? "block" : "none";
+    });
+
+    slides.forEach((slide, index) => {
+        slide.addEventListener("click", () => {
+            slides.forEach(item => item.classList.remove("active"));
+            catalogs.forEach(item => item.style.display = "none");
+
+            slide.classList.add("active");
+
+            if (catalogs[index]) {
+                catalogs[index].style.display = "block";
+            }
+        });
+    });
+
+    // Табы моделей
+    document.querySelectorAll(".tabs").forEach(tab => {
+        const buttons = tab.querySelectorAll(".tabs__btns > div");
+        const areas = tab.querySelectorAll(".tabs__area");
+
+        // Инициализация
+        areas.forEach((area, index) => {
+            area.style.display = index === 0 ? "flex" : "none";
+        });
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                buttons.forEach(item => item.classList.remove("active"));
+                btn.classList.add("active");
+
+                areas.forEach(area => {
+                    area.style.display = "none";
+                });
+
+                if (areas[index]) {
+                    areas[index].style.display = "flex";
+                }
+            });
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  let menuBtn = document.querySelector('.menu-btn');
+  let menu = document.querySelector('.menu');
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+});
+
+// Замена <img class="svg"> на inline SVG
+document.addEventListener("DOMContentLoaded", () => {
+  const svgImages = document.querySelectorAll('img.svg');
+
+  svgImages.forEach(img => {
+    const imgURL = img.getAttribute('src');
+
+    fetch(imgURL)
+      .then(response => response.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(data, 'image/svg+xml');
+        let svg = xmlDoc.querySelector('svg');
+
+        if (!svg) return;
+
+        // Перенос ID
+        if (img.id) {
+          svg.setAttribute('id', img.id);
+        }
+
+        // Перенос классов
+        const classes = img.getAttribute('class');
+        if (classes) {
+          svg.setAttribute('class', `${classes} replaced-svg`);
+        }
+
+        // Удаление некорректных xmlns
+        svg.removeAttribute('xmlns:a');
+
+        // Добавление viewBox, если его нет
+        if (!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
+          svg.setAttribute('viewBox', `0 0 ${svg.getAttribute('width')} ${svg.getAttribute('height')}`);
+        }
+
+        // Замена <img> на <svg>
+        img.parentNode.replaceChild(svg, img);
+      })
+      .catch(error => {
+        console.error(`Ошибка при загрузке SVG: ${imgURL}`, error);
+      });
+  });
+});
+
